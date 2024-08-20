5890789063bd47289f447172816b3de6b51ce738
Ajey Gore <ajey@soracloud.ai>
2024-08-20 03:18:23 +0000

    Fixing test, removed useless tests

--- spec/routing/api_tokens_routing_spec.rb
+++ spec/routing/api_tokens_routing_spec.rb
@@ -9,30 +9,5 @@ RSpec.describe ApiTokensController, type: :routing do
     it "routes to #new" do
       expect(get: "/api_tokens/new").to route_to("api_tokens#new")
     end
-
-    it "routes to #show" do
-      expect(get: "/api_tokens/1").to route_to("api_tokens#show", id: "1")
-    end
-
-    it "routes to #edit" do
-      expect(get: "/api_tokens/1/edit").to route_to("api_tokens#edit", id: "1")
-    end
-
-
-    it "routes to #create" do
-      expect(post: "/api_tokens").to route_to("api_tokens#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/api_tokens/1").to route_to("api_tokens#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/api_tokens/1").to route_to("api_tokens#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/api_tokens/1").to route_to("api_tokens#destroy", id: "1")
-    end
   end
 end
