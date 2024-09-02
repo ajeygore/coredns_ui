5ed241a6c8633b16faf5ac1e07e60a3e5839e4f1
Ajey Gore <ajey@soracloud.ai>
2024-09-02 06:25:08 +0000

    Fixing anonymous domains

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   skip_before_action :require_login, only: %i[create destroy]
 
   def create
+    auth = request.env['omniauth.auth']
+    unless auth.info.email.split('@').last == 'soracloud.ai'
+      redirect_to root_path, alert: 'You are not authorized, please contact admins.'
+      return
+    end
     @user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = @user.id
     redirect_to root_path
