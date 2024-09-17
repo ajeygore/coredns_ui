a9182abbcb5c309d668e2b78a479bc37c5490a5c
Ajey Gore <ajey@soracloud.ai>
2024-09-17 02:46:34 +0000

    making production deploy scripts

--- scripts/rails-prod.sh
+++ scripts/rails-prod.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
+
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# ----------------------------
+# Configuration Variables
+# ----------------------------
+
+# List of required environment variables
+REQUIRED_VARS=("DATABASE_USER" "DATABASE_PASSWORD" "DATABASE" "APP_PORT" "DEPLOY_USER")
+
+# Function to check environment variables
+check_env_vars() {
+  local missing_vars=()
+  
+  for var in "${REQUIRED_VARS[@]}"; do
+    if [[ -z "${!var}" ]]; then
+      missing_vars+=("$var")
+    fi
+  done
+  
+  if [ ${#missing_vars[@]} -ne 0 ]; then
+    echo "Error: The following environment variable(s) are not set: ${missing_vars[*]}"
+    exit 1
+  fi
+}
+
+# Check required environment variables
+check_env_vars
+
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y ansible wget curl rsync git
-
-
-sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-ansible-playbook scripts/rails-prod.yml
+sudo apt-get install -y ansible wget curl rsync git libssl-dev libreadline-dev zlib1g-dev libffi-dev libffi8 build-essential libyaml-dev
 
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+# Install Ruby
+if command -v ruby >/dev/null 2>&1; then
+  INSTALLED_RUBY_VERSION=$(ruby -v | awk '{print $2}')
+  echo "Ruby is installed. Version: $INSTALLED_RUBY_VERSION"
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-rbenv install 3.3.4
+else
+  echo "Ruby is not installed. Proceeding with installation."
+
+  cd ~
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  sudo rm -rf ruby-3.3.4.tar ruby-3.3.4
+  wget "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.4.tar.gz"
+  tar -xzvf ruby-3.3.4.tar.gz
+  cd ruby-3.3.4
+  ./configure --disable-install-rdoc --enable-yjit --with-libffi-dir=/usr/lib/x86_64-linux-gnu
+  make
+  sudo make install
+fi
 
 
-# No docker required yet
+
+
+
+# # No docker required yet
 # sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 # sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -46,11 +87,7 @@ source ~/.bashrc
 export GEM_HOME=~/.ruby/
 export PATH="$PATH:~/.ruby/bin"
 
+cd ~/coredns_ui
 
-GEM_HOME=~/.ruby/ PATH="$PATH:~/.ruby/bin" gem install bundler
-rm -rf .local
-bundle config set --local path '.local'
-GEM_HOME=~/.ruby/ PATH="$PATH:~/.ruby/bin" bundle install
- 
-# verify that docker is installed properly
-# sudo docker run hello-world
+
+GEM_HOME=~/.ruby/ PATH="$PATH:~/.ruby/bin" ansible-playbook scripts/rails_prod.yml
